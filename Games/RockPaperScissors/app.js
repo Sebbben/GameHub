9ede575403aa04fcfa870b5ea3cc8dda580f95df
Nixinic <nicolai.ljungmann@gmail.com>
2020-01-12 21:32:58 +0100

    stein saks p

--- Games/RockPaperScissors/app.js
+++ Games/RockPaperScissors/app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const AIScore = 0;
+let userScore = 0;
+let AIScore = 0;
 const userScore_span = document.getElementById("user-score");
 const AIScore_span = document.getElementById("AI-score");
 const scoreboard_ = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -11,14 +11,65 @@ const scissors_div = document.getElementById("s");
 
 function getAIChoice() {
     const choices = ['r','p','s'];
-    const randomNumber = Math.floor(Math.random() = 3);
+    const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 getAIChoice();
 
-function game(userChoice){
-
+function convertToWord(letter) {
+    if (letter === "r") return rock;
+    if (letter === "p") return paper;
+   else return "scissors";
 }
+
+function win(userChoice, AIChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    AIScore_span.innerHTML = AIScore;
+    const smalluserWord = "(user)".fontsize(3);
+    const smallAIWord = "(AI)".fontsize(3);
+    result_p.innerHTML = userChoice + (smalluserWord) + "Beats" + AIScore + (smallAIWord) + ". You win!";
+    
+}
+
+function lose(userChoice, AIChoice) {
+    AIScore++;
+    userScore_span.innerHTML = userScore;
+    AIScore_span.innerHTML = AIScore;
+    const smalluserWord = "(user)".fontsize(3);
+    const smallAIWord = "(AI)".fontsize(3);
+    result_p.innerHTML =  AIScore + (smallAIWord) + "Beats" + userChoice + (smalluserWord)  + ". You lose!";
+    
+}
+
+function draw(userChoice, AIChoice) {
+    const smalluserWord = "(user)".fontsize(3);
+    const smallAIWord = "(AI)".fontsize(3);
+    result_p.innerHTML = userChoice + (smalluserWord) + "draws with" + AIScore + (smallAIWord) + ". It's a draw!";
+}
+
+function game(userChoice){
+    const AIChoice = getAIChoice();
+    switch (userChoice + AIChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, AIChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, AIChoice);
+        break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, AIChoice);
+            break;
+            
+    }}
+    
+
 
 function main() {
     rock_div.addEventListener('click', function() {
@@ -32,3 +83,4 @@ function main() {
                 })
 }
     
+main ();
