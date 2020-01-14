ff070727ee2e784d59c9bb5e1f80a4127b6099af
Sebbbens <sebastian.jossang@gmail.com>
2020-01-14 07:35:56 +0100

    Endringer

--- Games/RockPaperScissors/app.js
+++ Games/RockPaperScissors/app.js
@@ -8,27 +8,23 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-
 function getAIChoice() {
-    const choices = ['r','p','s'];
-    const randomNumber = Math.floor(Math.random() = 3);
-    return choices[randomNumber];
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
 }
 getAIChoice();
 
-function game(userChoice){
-
-}
+function game(userChoice) {}
 
 function main() {
-    rock_div.addEventListener('click', function() {
-        game("r");
-        })
-    paper_div.addEventListener('click', function() {
-        game("p");
-            })
-    scissors_div.addEventListener('click', function() {
-        game("s");
-                })
+  rock_div.addEventListener("click", function() {
+    game("r");
+  });
+  paper_div.addEventListener("click", function() {
+    game("p");
+  });
+  scissors_div.addEventListener("click", function() {
+    game("s");
+  });
 }
-    
