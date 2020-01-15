acd12d2f01d914da407a861d2d1d62f98549b272
Jøssang <sebjoe@365.nesodden.kommune.no>
2020-01-15 16:58:15 +0100

    Have begun on the simon game

--- Games/ColorMemory/script.js
+++ Games/ColorMemory/script.js
@@ -1,1 +1,67 @@
-var temp;
+const topLeft = document.getElementById("top-left");
+const topRight = document.getElementById("top-right");
+const bottomLeft = document.getElementById("bottom-left");
+const bottomRight = document.getElementById("bottom-right");
+var colors = [];
+var currentIndex = 0;
+
+function lightUp(color) {
+  if (color == "green") {
+    topLeft.style.opacity = 0.5;
+    setTimeout(function () {
+      topLeft.style = "";
+    }, 500);
+  } else if (color == "red") {
+    topRight.style.opacity = 0.5;
+    setTimeout(function () {
+      topRight.style = "";
+    }, 500);
+  } else if (color == "yellow") {
+    bottomLeft.style.opacity = 0.5;
+    setTimeout(function () {
+      bottomLeft.style = "";
+    }, 500);
+  } else {
+    bottomRight.style.opacity = 0.5;
+    setTimeout(function () {
+      bottomRight.style = "";
+    }, 500);
+  }
+}
+
+function main(pickedColor) {
+  if (pickedColor == colors[currentIndex]) {
+    currentIndex++;
+    console.log(currentIndex)
+    i = 0;
+    if (currentIndex == colors.length) {
+      currentIndex = 0;
+      newColor()
+      while (i < colors.length) {
+        setTimeout(() => {
+          lightUp(colors[i])
+          i++;
+        }, 200);
+      }
+    }
+  }
+}
+
+function newColor() {
+  var randNum = Math.ceil(Math.random() * 4)
+  if (randNum == 1) {
+    randNum = "green"
+  } else if (randNum == 2) {
+    randNum = "red"
+  } else if (randNum == 3) {
+    randNum = "yellow"
+  } else {
+    randNum = "blue"
+  }
+  colors.push(randNum)
+}
+
+function startGame() {
+  newColor()
+  lightUp(colors[0])
+}
