a34d3a89f63de951a0553257fd10534fe87efd27
Sebbbens <sebastian.jossang@gmail.com>
2020-01-16 07:52:42 +0100

    tried to fix simon game

--- Games/ColorMemory/script.js
+++ Games/ColorMemory/script.js
@@ -8,22 +8,22 @@ var currentIndex = 0;
 function lightUp(color) {
   if (color == "green") {
     topLeft.style.opacity = 0.5;
-    setTimeout(function () {
+    setTimeout(function() {
       topLeft.style = "";
     }, 500);
   } else if (color == "red") {
     topRight.style.opacity = 0.5;
-    setTimeout(function () {
+    setTimeout(function() {
       topRight.style = "";
     }, 500);
   } else if (color == "yellow") {
     bottomLeft.style.opacity = 0.5;
-    setTimeout(function () {
+    setTimeout(function() {
       bottomLeft.style = "";
     }, 500);
   } else {
     bottomRight.style.opacity = 0.5;
-    setTimeout(function () {
+    setTimeout(function() {
       bottomRight.style = "";
     }, 500);
   }
@@ -32,36 +32,36 @@ function lightUp(color) {
 function main(pickedColor) {
   if (pickedColor == colors[currentIndex]) {
     currentIndex++;
-    console.log(currentIndex)
+    console.log(currentIndex);
     i = 0;
-    if (currentIndex == colors.length) {
-      currentIndex = 0;
-      newColor()
-      while (i < colors.length) {
+    function displayColors(i) {
+      if (i < colors.length) {
         setTimeout(() => {
-          lightUp(colors[i])
           i++;
-        }, 200);
+          lightUp(colors[i]);
+          displayColors(i);
+        }, 500);
       }
     }
+    displayColors(0);
   }
 }
 
 function newColor() {
-  var randNum = Math.ceil(Math.random() * 4)
+  var randNum = Math.ceil(Math.random() * 4);
   if (randNum == 1) {
-    randNum = "green"
+    randNum = "green";
   } else if (randNum == 2) {
-    randNum = "red"
+    randNum = "red";
   } else if (randNum == 3) {
-    randNum = "yellow"
+    randNum = "yellow";
   } else {
-    randNum = "blue"
+    randNum = "blue";
   }
-  colors.push(randNum)
+  colors.push(randNum);
 }
 
 function startGame() {
-  newColor()
-  lightUp(colors[0])
+  newColor();
+  lightUp(colors[0]);
 }
