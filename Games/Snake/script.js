9987aaf090b298e6dc1cf2c92a845bd19cc128b3
Sebbben <sebastian.jossang@gmail.com[A>
2020-02-18 14:05:20 +0100

    stuffs

--- Games/Snake/script.js
+++ Games/Snake/script.js
@@ -1,1 +1,36 @@
-var temp;
+var mainContainer = document.getElementById("main-container");
+var size = 16;
+var length = 1;
+var gameOver = false;
+var playerX = 0;
+var playerY = 0;
+var dir = "left";
+var snake = [];
+
+class snakePeace {
+  constructor(x, y, dur) {
+    this.x = x;
+    this.y = y;
+    this.dur = dur;
+  }
+}
+
+function getLonger() {
+  snake.push(new snakePeace(x, y, length));
+  for(i=)
+}
+
+for (i = 0; i < size; i++) {
+  var row = document.createElement("div");
+  for (j = 0; j < size; j++) {
+    var div = document.createElement("div");
+    div.classList.add("playArea");
+    div.value = 1;
+    row.append(div);
+  }
+  mainContainer.append(row);
+}
+
+while (!gameOver) {
+  gameOver = true;
+}
