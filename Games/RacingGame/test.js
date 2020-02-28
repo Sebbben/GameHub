7e98e83a849e13555b1e8491b54d6a800a042090
nixinic <nicolai.ljungmann@gmail.com>
2020-02-28 16:06:08 +0100

    Mekka ny side

--- Games/RacingGame/test.js
+++
@@ -1,7 +0,0 @@
-function makePath() {
-  path = [[0, 0]];
-  for (i = 0; i < 7; i++) {
-    var randDist = Math.ceil(Math.random() * 3);
-    path.push([path[path.length - 1][1] + randDist, path[path.length - 1][0]]);
-  }
-}
